64e99b33b686d7e11ee8e1b6606fde7b5ae0bb6c
Geoffrey Graddy <geoffrey.graddy@gmail.com>
2022-12-13 18:15:20 -0500

    Add Inventory Item
    Added the ability to add an inventory item to the database.

--- src/main/java/com/homeinventory/backend/controllers/HomeInventoryController.java
+++ src/main/java/com/homeinventory/backend/controllers/HomeInventoryController.java
@@ -3,9 +3,7 @@ package com.homeinventory.backend.controllers;
 import com.homeinventory.backend.models.InventoryItem;
 import com.homeinventory.backend.repositories.InventoryItemJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +15,13 @@ public class HomeInventoryController {
     private InventoryItemJpaRepository jpaRepository;
 
     @GetMapping("")
-    public List<InventoryItem> listRecipes(){
+    public List<InventoryItem> listInventoryItems(){
         return jpaRepository.findAll();
+    }
+
+    @PostMapping("")
+    public InventoryItem createInventoryItem(@RequestBody InventoryItem inventoryItem){
+        return jpaRepository.save(inventoryItem);
     }
 
 }
